a9fbcd5d6918528af8051e25fb1bd804eefb78b8
samani <samani@mail.ugm.ac.id>
2017-04-25 10:03:27 +0700

    init

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+function getKey() {
+   $keyRS = "970f66da51daff170991e0e9a37910f841941eb42470f39e47310643212d1b1d";   //Kunci Enkripsi Dari Eklaim
+   return $keyRS;
+}
+
+function getUrlWS() {
+	$UrlWS = "http://192.168.1.19/E-Klaim/ws.php";                         //IP SERVER EKLAIM
+	return $UrlWS;
+}
+
+?>
